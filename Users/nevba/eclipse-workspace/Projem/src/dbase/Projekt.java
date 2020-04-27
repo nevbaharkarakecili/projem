9787500a209ca41ca7dfc7259c9c826550ea8469
nevba <nevba@DESKTOP-8FHUI26>
2020-04-27 15:45:09 +0300

    Hopefully first commit

---
+++ Users/nevba/eclipse-workspace/Projem/src/dbase/Projekt.java
@@ -0,0 +1,34 @@
+package dbase;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+//import com.mysql.jdbc.Connection;
+
+public class Projekt {
+
+	public static void main(String[] args) {
+
+		try {
+
+			
+			Connection myConn = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/projekt?useUnicode=true&useLegacyDatetimeCode=false&serverTimezone=Turkey","root","1234");
+			Statement myStat = (Statement) myConn.createStatement();
+			ResultSet myRs =myStat.executeQuery("select* from mitarbeiter");
+			while(myRs.next()) {
+			System.out.print(myRs.getString("mitarb_operator")+" - "+myRs.getString("mitarb_degerlendiren")+" - "+ myRs.getString("mitarb_onay")+" - "+myRs.getString("mitarb_customer"));
+
+			}
+		} 
+		
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+
+}
