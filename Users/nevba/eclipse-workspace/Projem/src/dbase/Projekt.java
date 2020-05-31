a7d331fa8ff183fd188e4a7e071356f2f0d3c25b
nevba <nevba@DESKTOP-8FHUI26>
2020-05-31 17:45:54 +0300

    java fx und scene builder anbindung

--- Users/nevba/eclipse-workspace/Projem/src/dbase/Projekt.java
+++ Users/nevba/eclipse-workspace/Projem/src/dbase/Projekt.java
@@ -17,10 +17,15 @@ public class Projekt {
 			Connection myConn = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/projekt?useUnicode=true&useLegacyDatetimeCode=false&serverTimezone=Turkey","root","1234");
 			Statement myStat = (Statement) myConn.createStatement();
 			ResultSet myRs =myStat.executeQuery("select* from mitarbeiter");
-			while(myRs.next()) {
-			System.out.print(myRs.getString("mitarb_operator")+" - "+myRs.getString("mitarb_degerlendiren")+" - "+ myRs.getString("mitarb_onay")+" - "+myRs.getString("mitarb_customer"));
+			/*
+			 * 
+			 * while(myRs.next()) {
+			System.out.print(myRs.getString("personel_bilgi")+" - "+myRs.getString("adi_soyadi")+" - "+ myRs.getString("seviye")+" - "+myRs.getString("tarih")+myRs.getString("imza"));
 
 			}
+			*
+			*
+			*/
 		} 
 		
 		catch(Exception e) {
